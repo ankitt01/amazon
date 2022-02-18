dcf1e99c91d338542a72d12e9c9df906e5672b45
ankitt01 <ankitrawat0102@gmail.com>
2022-02-18 14:58:58 +0530

    completed the product component

---
+++ components/ProductFeed.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Product from './Product'
+
+const ProductFeed = ({products}) => {
+  return (
+    <div>
+        <h1>Products here...</h1>
+        {products.map(({id,title,price,description,category,image}) => {
+            return <Product key={id} id={id} title={title} price={price} description={description} category={category} image={image} />
+        })}
+    </div>
+  )
+}
+
+export default ProductFeed
